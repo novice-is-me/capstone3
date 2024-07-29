4635546c39d96ab9f18938e6342426c31ad8fe4a
Regina Jalandoni <reginajalandoni13@gmail.com>
2024-07-29 12:28:19 +0800

    Update the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react'
 import { Button } from 'react-bootstrap'
 import UserContext from '../UserContext'
-import { Navigate } from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom';
 
 const Home = () => {
 
@@ -11,8 +11,8 @@ const Home = () => {
     <div className=' text-center my-5'>
         <h1 className=' fs-1 color-secondary my-5'>Welcome to ECommerce API Website</h1>
         {(user.id === null && user.id === null) ?
-          <Button href="/login" variant="primary">View Products</Button>
-        : <Button href="/products" variant="primary">View Products</Button>
+          <Button to="/login" variant="primary" as={Link}>View Products</Button>
+        : <Button to="/products" variant="primary" as={Link}>View Products</Button>
         }
     </div>
   )
