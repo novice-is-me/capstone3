33b56b6ecee9036761651d6d6dbd4591c0501f0d
Regina Jalandoni <reginajalandoni13@gmail.com>
2024-07-29 12:22:23 +0800

    Update codes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Button } from 'react-bootstrap'
+import UserContext from '../UserContext'
+import { Navigate } from 'react-router-dom';
 
 const Home = () => {
+
+  const { user } = useContext(UserContext);
+
   return (
     <div className=' text-center my-5'>
         <h1 className=' fs-1 color-secondary my-5'>Welcome to ECommerce API Website</h1>
-        <Button href="/products" variant="primary">View Products</Button>
+        {(user.id === null && user.id === undefined) ?
+          <Button href="/login" variant="primary">View Products</Button>
+        : <Button href="/products" variant="primary">View Products</Button>
+        }
     </div>
   )
 } 
