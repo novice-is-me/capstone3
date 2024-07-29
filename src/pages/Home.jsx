e5fd708d227656711e9b8c742b528f4d888cff8e
Regina Jalandoni <reginajalandoni13@gmail.com>
2024-07-29 12:24:43 +0800

    Update codes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
   return (
     <div className=' text-center my-5'>
         <h1 className=' fs-1 color-secondary my-5'>Welcome to ECommerce API Website</h1>
-        {(user.id === null && user.id === undefined) ?
+        {(user.id === null && user.id === null) ?
           <Button href="/login" variant="primary">View Products</Button>
         : <Button href="/products" variant="primary">View Products</Button>
         }
